695c1c460b2f295773f05a9d3ea9d3afef971974
igwemiracle <igwemiracle35@gmail.com>
2023-02-17 00:22:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,8 @@
   * Description: main - Programming is like building a multilingual puzzle followed by a newline
   * Return: 0 if success
   */
-
-
 int main(void)
 {
-        puts("/"Programming is like building a multilingual puzzle\n");
-	return 0;
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
