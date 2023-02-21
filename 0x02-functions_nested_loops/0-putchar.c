2767838c65198821579d9eafc7328c399b7f19a7
igwemiracle <igwemiracle35@gmail.com>
2023-02-21 02:52:03 -0800

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints "_putchar" with the putchar function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int letter;
+
+	letter = '_';
+	putchar(letter);
+	letter = 'p';
+	putchar(letter);
+	letter = 'u';
+	putchar(letter);
+	letter = 't';
+	putchar(letter);
+	letter = 'c';
+	putchar(letter);
+	letter = 'h';
+	putchar(letter);
+	letter = 'a';
+	putchar(letter);
+	letter = 'r';
+	putchar(letter);
+
+	putchar('\n');
+	return (0);
+}
