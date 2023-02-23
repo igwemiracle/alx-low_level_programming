5cabc0932a7ffbe955b3c145974ec53784014545
igwemiracle <igwemiracle35@gmail.com>
2023-02-23 13:42:41 -0800

    0-isup

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 1 on success.
+ * On error, -1 is returned, and erron is set approximately.
+ */
+
+int _putchar(char c)
+
+{
+
+        return (write(1, &c, 1));
+
+}
