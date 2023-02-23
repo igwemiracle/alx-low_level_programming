1a693709f49efbe4fbad4b3f00f9a5e5183c2bb4
igwemiracle <igwemiracle35@gmail.com>
2023-02-23 01:57:09 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
@@ -30,4 +30,3 @@ int largest_number(int a, int b, int c)
 	}
 	return (largest);
 }
-
