cb6652d6e2e53ce28e6be9d0431c597ec6af4e6e
igwemiracle <igwemiracle35@gmail.com>
2023-03-06 19:24:54 -0800

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: the value to modify
+ * @to: the value to assign
+ * Return: Always 0 (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
