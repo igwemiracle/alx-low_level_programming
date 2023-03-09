eed9f1b69071a13eb6f0631103cd8835b391b208
igwemiracle <igwemiracle35@gmail.com>
2023-03-09 03:12:05 -0800

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -11,5 +8,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
-#endif /* HOLBERTON_H */
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
