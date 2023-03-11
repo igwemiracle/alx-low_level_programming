20192dc72df73b9adee3075f0d1274dbf6096227
igwemiracle <igwemiracle35@gmail.com>
2023-03-11 05:12:54 -0800

    mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - A program the prints its name -
+ * @argc: argument count
+ * @argv: array pointing to arguements
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
