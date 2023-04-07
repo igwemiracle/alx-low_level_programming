0776619eb5623bae99caa0189841e64bc39778f8
igwemiracle <igwemiracle35@gmail.com>
2023-04-07 15:43:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,35 +1,41 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * create_file - function will create a file
- * @filename:  name of file to be created
- * @text_content: text to be written in new file
- * Return: function will return 1 on success, -1 on fail
+ * create_file - creates a file and puts text in it
+ * with 600 perms (do not change if it exists)
+ *
+ * @filename: name for file
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i = 0, writer;
+	int fd, bytes_written;
 
 	if (filename == NULL)
-		return (-1);
-
-	if (text_content)
 	{
-		fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
-		if (fd == -1)
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
 			return (-1);
+		}
 	}
 
-	text_content = "";
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-	writer = write(fd, text_content, i);
-	if (writer == -1)
-		return (-1);
-
+	close(fd);
 	return (1);
 }
